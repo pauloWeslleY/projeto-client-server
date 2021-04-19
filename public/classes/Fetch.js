7fee765dd2665a051bd802f696bfb786b71a2c74
pauloWeslleY <weslleyspfc13@outlook.com>
2021-04-19 01:17:19 -0300

    upload file Fetch.js

---
+++ public/classes/Fetch.js
@@ -0,0 +1,54 @@
+class Fetch {
+
+   static get(url, params = {}) {
+
+      return Fetch.request('GET', url, params);
+
+   }
+   static delete(url, params = {}) {
+
+      return Fetch.request('DELETE', url, params);
+
+   }
+   static put(url, params = {}) {
+
+      return Fetch.request('PUT', url, params);
+
+   }
+   static post(url, params = {}) {
+
+      return Fetch.request('POST', url, params);
+
+   }
+   // todo Método static permite q a gente chame ele diretamente sem ter que cria uma instância!!!!
+   static request(method, url, params = {}) {
+
+      return new Promise((resolve, reject) => {
+         let request;
+         switch (method.toLowerCase()) {
+            case 'get':
+               request = url;
+               break;
+            default:
+               request = new Request(url, {
+                  method,
+                  body: JSON.stringify(params),
+                  headers: new Headers({
+                     'Content-Type': 'application/json'
+                  })
+               });
+         }
+         fetch(request).then(response => {
+            response.json().then(json => {
+               resolve(json);
+            }).catch(e => {
+               reject(e);
+            });
+         }).catch(err => {
+            reject(err);
+         });
+      });
+
+   }
+
+}
